71fa99255795a664228060e0e31b0479bde7a73d
farhad <farhadhossain0085@gmail.com>
2020-04-26 22:17:44 +0600

    arduino File Updated

--- Arduino/arduinoSendData.ino
+++ Arduino/arduinoSendData.ino
@@ -1,3 +1,5 @@
+String data[8] = {"a2a3", "b2b4", "c2c4", "d2d3", "e2e4", "f2f3", "g2g3", "h2h4"};
+
 void setup()
 {
   Serial.begin(9600);
@@ -5,7 +7,10 @@ void setup()
 
 void loop()
 {
-  Serial.println("a2a3");
-  delay(1000);
-  Serial.flush();
+  for(int i = 0; i<8; i++){
+    Serial.println(data[i]);
+    delay(1000);
+    Serial.flush();
+  }
+  
 }
